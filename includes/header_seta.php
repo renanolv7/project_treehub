23e2f762017797f5e5f84baa51304539f44274e5
renanolv7 <reviniciusoliveira@gmail.com>
2025-05-06 01:56:58 -0300

    feat:"include header criado"

---
+++ includes/header_seta.php
@@ -0,0 +1,6 @@
+
+<header class="w-full mb-10">
+        <div class="btn-arrow" flex justify-center>
+            <img class="w-14 h-14 cursor-pointer" src="../assets/images/seta-esquerda.png" alt="Seta voltar">
+        </div>
+</header>
