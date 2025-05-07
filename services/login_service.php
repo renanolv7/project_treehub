3701c70ad0044a9d7cb5448d170e028ff91370aa
pauloo163 <paulo.klapowskadevargas@gmail.com>
2025-05-07 19:44:32 -0300

    Refactor(login): adicionado conexao ao banco, e verificação de login (nao testado

---
+++ services/login_service.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "treehub";
+
+    $connection = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "SELECT * FROM usuarios where email = '$email' and senha = SHA('$senha')";
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+        session_start();
+        $_SESSION['logged_in'] = true;
+        header("Location: ../../index.php");
+    } else {
+        header("Location: ../login.php");
+    }
+}
+?>
