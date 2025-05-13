8cd436a723d5bea1c0754f14d35f619e8c6036c5
renanolv7 <reviniciusoliveira@gmail.com>
2025-05-13 19:00:54 -0300

    refactor(Contato): codigo refatorado

--- pages/contato.php
+++ pages/contato.php
@@ -24,28 +24,29 @@ require_once __DIR__ . '/../config/urls.php';
     
         <div class="flex justify-stretch items-center w-full">
             <div class="logo">
-                <a href=""><img class="w-24" src="<?= IMAGENS ?>logo-tree-hub.png" alt="Logo" srcset=""></a>
+                <a href="<?= INICIO ?>index.php"><img class="w-24" src="<?= IMAGENS ?>logo-tree-hub.png" alt="Logo" srcset=""></a>
             </div>
         
             <nav class="hidden md:flex items-center space-x-6">
                 <a href="<?= INICIO ?>index.php" class="">Inicio</a>
-                <a href="<?= ICONES ?>" class="">Sobre</a>
-                <a href="<?= ICONES ?>" class="">Metas</a>
-                <a href="<?= ICONES ?>" class="">Contato</a>
+                <a href="#" class="">Sobre</a>
+                <a href="#" class="">Metas</a>
+                <a href="#" class="">Contato</a>
             </nav>
             
             <div class="hidden md:block w-1/4 h-px bg-gray-900"></div>
 
             <div class="hidden md:flex space-x-4">
-                <a href="" class="social-instagram w-5"><img src="<?= ICONES ?>instagram.png" alt="" srcset=""></a>
-                <a href="" class="social-facebook w-5"><img src="<?= ICONES ?>facebook.png" alt="" srcset=""></a>
-                <a href="" class="social-other w-5"><img src="<?= ICONES ?>instagram.png" alt="" srcset=""></a>
+                <a href="#" class="social-instagram w-5"><img src="<?= ICONES ?>instagram.png" alt="" srcset=""></a>
+                <a href="#" class="social-facebook w-5"><img src="<?= ICONES ?>facebook.png" alt="" srcset=""></a>
+                <a href="#" class="social-other w-5"><img src="<?= ICONES ?>icone_partilhar.png" alt="" srcset=""></a>
             </div>
 
         </div>
 
         <div class="flex justify-evenly items-end bg-white w-80 rounded-t-lg mt-3 mr-3">
-            <button class="border-2 border-[#02300b] px-6 py-1 rounded-lg font-semibold">APOIE A CAUSA</button>
+            <a href="<?= CADASTRO ?>cadastro.php"><button class="border-2 border-[#02300b] px-6 py-1 rounded-lg font-semibold">APOIE A CAUSA</button></a>
+            
             <a class="items-center" href=""><img src="<?= ICONES ?>do-utilizador.png"></a>
        
         </div>
@@ -63,6 +64,7 @@ require_once __DIR__ . '/../config/urls.php';
             </section>
     
             <section class="w-full md:w-1/2 p-8">
+
                 <form class="flex items-center" id="form-contato" action="" method="post">
         
                     <div class="w-[30rem] h-fit bg-[#fffefe] rounded-2xl m-5 p-8 shadow-xl">
@@ -102,7 +104,8 @@ require_once __DIR__ . '/../config/urls.php';
                                           placeholder="Escreva sua mensagem"
                                           required
                                           resize=none
-                                          ></textarea>
+                                          >
+                                </textarea>
                             </div>
                         </div>
                         
@@ -116,6 +119,7 @@ require_once __DIR__ . '/../config/urls.php';
                     </div>
         
                 </form>
+
             </section>
         </div>
         
